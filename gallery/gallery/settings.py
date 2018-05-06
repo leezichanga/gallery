7db0948d32c3bb40b16bce84e8b29b41715d268d
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-06 12:43:34 +0300

    adding search button

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos',
+    # 'pictures',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
