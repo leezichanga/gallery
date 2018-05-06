a7bde68c3687a182fe40dcc3000abf38047e021b
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-06 14:21:46 +0300

    installed pillow

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'pictures',
+    'pictures',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ gallery/pictures/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+ url(r'^$', views.my_gallery, name='myGallery'),
+ url(r'^photo/(\d+)', views.single_photo, name = 'myPhoto'),
+ url(r'^search/', views.search_results, name = 'search_results'),
+]
