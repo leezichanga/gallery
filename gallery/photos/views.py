1ce6faa4855b3100e6ffe0003a82aa1262286254
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-06 11:23:24 +0300

    link navbar to base.html

--- gallery/photos/views.py
+++ gallery/photos/views.py
@@ -6,47 +6,6 @@ import datetime as dt
 def welcome(request):
     return HttpResponse('Welcome to the My Gallery')
 
-
-def photos_of_day(request):
-    date = dt.date.today()
-
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def past_days_photos(request,past_date):
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+def my_gallery(request):
+    photos = Photos.objects.all()
+    return render(request, 'gallery.html', {'photos':photos})
